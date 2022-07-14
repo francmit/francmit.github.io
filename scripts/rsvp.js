b06f73f2a7ac92e5d5027f004589bf7c7ba7bfb6
francmit <mitchell.francisco1@gmail.com>
2022-07-13 20:24:48 -0700

    Add reveal animation to our story

--- scripts/rsvp.js
+++ scripts/rsvp.js
@@ -118,4 +118,23 @@ document.addEventListener('DOMContentLoaded', function() {
     document.querySelector("#verify").onclick = function () {
         authenticate(document.querySelector('#firstName').value , document.querySelector('#lastName').value );
     };
+    window.addEventListener("scroll", reveal);
 }, false);
+
+function reveal() {
+    var reveals = document.querySelectorAll(".reveal");
+  
+    for (var i = 0; i < reveals.length; i++) {
+      var windowHeight = window.innerHeight;
+      var elementTop = reveals[i].getBoundingClientRect().top;
+      var elementVisible = 150;
+  
+      if (elementTop < windowHeight - elementVisible) {
+        reveals[i].classList.add("active");
+      } else {
+        reveals[i].classList.remove("active");
+      }
+    }
+  }
+  
+  
